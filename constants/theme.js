ff2d2c74ce070d1a48328a361f5f46226d99b08b
Yash Naik <ynaik366@gmail.com>
2025-03-03 15:47:52 +0530

    first commit

---
+++ constants/theme.js
@@ -0,0 +1,11 @@
+export const theme ={
+    colors:{
+        blue: '#d7e3fc', 
+        lightblue: '#153ae0',
+        darkblue: '#1b263b',
+        white: '#ffffff',
+        black: '#000000' ,
+        paleorange: '#fae1dd',
+        paleyellow: '#fff2b2'
+    }
+}
