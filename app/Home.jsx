376e541a19ca109ae3e6b2f5207e22e97ebe073c
Samuel-for-github <samuelfernandes10001@gmail.com>
2025-03-10 16:43:17 +0530

    install

--- app/Home.jsx
+++ app/Home.jsx
@@ -2,32 +2,26 @@ import { StyleSheet, Text, View, Button } from 'react-native'
 import React from 'react'
 
 import ScreenWrapper from '../components/ScreenWrapper'
-import {theme} from '../constants/theme'
-import {wp, hp} from '../helpers/common'
-import {useRouter} from 'expo-router'
-// import Secret from './secret.jsx';
-// import Video from './video';
+import { theme } from '../constants/theme'
+import { wp, hp } from '../helpers/common'
+import { useRouter } from 'expo-router'
 
 const Home = () => {
     const router = useRouter();
     return (
         <ScreenWrapper bg='#fff'>
-            
-            <View style={{alignItems: 'center'}}>
+            <View style={styles.container}>
                 <View style={styles.waterLevel}>
                     <Text style={styles.waterLevelText}>1290 L</Text>
                     <Text style={styles.remainingText}>Remaining: 603 L</Text>
                 </View>
-                {/* <Secret /> */}
-                <Button onPress={() => router.push('sections/secret')} title='Click'/>
-                {/* <Video /> */}
+                <Button onPress={() => router.push('sections/secret')} title='Click' />
 
-
-                <View style={{marginTop: hp(25),flexDirection: 'row', flexWrap: 'wrap', justifyContent: 'center'}}>
-                    <View style={{borderWidth: wp(1), width: wp(35), height: hp(15)}}><Text style={{fontSize: hp(10), color: 'black'}}>1</Text></View>
-                    <View style={{borderWidth: wp(1), width: wp(35), height: hp(15)}}><Text style={{fontSize: hp(10), color: 'black'}}>2</Text></View>
-                    <View style={{borderWidth: wp(1), width: wp(35), height: hp(15)}}><Text style={{fontSize: hp(10), color: 'black'}}>3</Text></View>
-                    <View style={{borderWidth: wp(1), width: wp(35), height: hp(15)}}><Text style={{fontSize: hp(10), color: 'black'}}>4</Text></View>
+                <View style={styles.waterItemsContainer}>
+                    <View style={styles.waterItem}><Text style={styles.waterItemText}>1</Text></View>
+                    <View style={styles.waterItem}><Text style={styles.waterItemText}>2</Text></View>
+                    <View style={styles.waterItem}><Text style={styles.waterItemText}>3</Text></View>
+                    <View style={styles.waterItem}><Text style={styles.waterItemText}>4</Text></View>
                 </View>
             </View>
         </ScreenWrapper>
@@ -37,16 +31,38 @@ const Home = () => {
 export default Home
 
 const styles = StyleSheet.create({
+    container: {
+        alignItems: 'center',
+    },
     waterLevel: {
         marginTop: wp(15),
-        alignItems: 'center'
+        alignItems: 'center',
     },
     waterLevelText: {
         color: theme.colors.darkblue,
-        fontSize: wp(15)
+        fontSize: wp(15),
     },
     remainingText: {
         color: theme.colors.darkblue,
-        fontSize: wp(3)
-    }
-})
+        fontSize: wp(3),
+    },
+    waterItemsContainer: {
+        marginTop: hp(25),
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        justifyContent: 'center',
+        gap: wp(8),
+    },
+    waterItem: {
+        alignItems: 'center',
+        borderRadius: wp(3),
+        backgroundColor: theme.colors.paleorange,
+        width: wp(35),
+        height: hp(15),
+        justifyContent: 'center',
+    },
+    waterItemText: {
+        fontSize: hp(10),
+        color: 'black',
+    },
+});
